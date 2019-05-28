c9c37d3660c69407714e871e9b61dfb48e05d026
Ma Xiaowen <Amber@Mas-MacBook-Pro-2.local>
2019-05-28 12:03:33 -0400

    modification

--- caption4sounds/api/main.py
+++ caption4sounds/api/main.py
@@ -17,11 +17,11 @@ def read_item(item_id: int, q: str = None):
 @app.get("/caption/{video_id}")
 def get_caption(video_id: str):
     filename, status = yt_audio_dl(video_id, 'temp_audio/')
-    wavefor = audio_load(path, filename)
+    wavefor = audio_load('temp_audio/', filename)
     sess_ckpt = prediction_utils.load_checkpoint('vggish_model.ckpt')
     vggish_features = prediction_utils.feature_extraction(wave,'vggish_pca_params.npz',sess_ckpt)
     block_10s = prediction_utils.block(vggish_features,10,2)
-    prediction = prediction_utils.model_prediction(path,'baseline_model',block_10s,256.,0.2)
-    labels = prediction_utils.prediction_label(path,'class_labels_indices.csv','display_name',prediction)
+    prediction = prediction_utils.model_prediction('temp_audio/','baseline_model',block_10s,256.,0.2)
+    labels = prediction_utils.prediction_label('temp_audio/','class_labels_indices.csv','display_name',prediction)
 
     return {"audio_filename": filename, "dl_status": 'finished', "results": labels}
