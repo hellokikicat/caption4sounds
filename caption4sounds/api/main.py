b914c55dc7108cace53fc63ed85bffe7676805d6
Ma Xiaowen <Amber@Mas-MacBook-Pro-2.local>
2019-05-28 11:32:33 -0400

    modified main.py

--- caption4sounds/api/main.py
+++ caption4sounds/api/main.py
@@ -20,12 +20,17 @@ def get_caption(video_id: str):
     filename, status = yt_audio_dl(video_id, 'temp_audio/')
     
 #     # load audio
-#     waveform = load_file(filename)
+wavefor = audio_load(pathname, filename)
 #     # go through vggish
+sess_ckpt = prediction_utils.load_checkpoint('vggish_model.ckpt')
 #     vggish_features = vggish_forward(waveform, sess)
+vggish_features = prediction_utils.feature_extraction(wave,'vggish_pca_params.npz',sess_ckpt)
 #     # hop batching
 #     vggish_batch = block(vggish_features)
+block_10s = prediction_utils.block(vggish_features,10,2)
 #     # last layers
+prediction = prediction_utils.model_prediction(path,'baseline_model',block_10s,256.,0.2)
+prediction_utils.prediction_label(path,'class_labels_indices.csv','display_name',prediction)
 #     preds = keras_convs(vggish_batch)
     
     return {"audio_filename": filename, "dl_status": 'finished'}
