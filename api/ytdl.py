7e24707a8a38272ce01e387722116e81517e3dc9
Ubuntu <ubuntu@ip-172-31-0-236.us-east-2.compute.internal>
2019-06-10 21:48:07 +0000

    detect downloaded audios

--- api/ytdl.py
+++ api/ytdl.py
@@ -1,6 +1,7 @@
 """ Utilities for downloading from YouTube"""
 
 from __future__ import unicode_literals
+from pathlib import Path
 import youtube_dl
 
 class MyLogger(object):
@@ -13,7 +14,6 @@ class MyLogger(object):
     def error(self, msg):
         print(msg)
 
-
 def yt_audio_dl(video_id, folderpath):
     """ main function for downloading audio from YouTube
     Always downloads the smallest audio file to save bandwidth, sample rate is enough
@@ -25,32 +25,36 @@ def yt_audio_dl(video_id, folderpath):
     Returns:
         path to the downloaded audio file and download status 
     """
-    filename = 'nullname'
-    status = 'not_started'
-    def finished_hook(d):
-        nonlocal filename
-        nonlocal status
-        if d['status'] == 'finished':
-#             print(filename)
-            filename = d['filename']
-            status = d['status']
-            print(f'Done downloading {filename}.')
-        
-    ydl_opts = {
-        'outtmpl': folderpath+'%(id)s.%(ext)s',
-        'format': 'worstaudio/worst',
-#         'postprocessors': [{
-#             'key': 'FFmpegExtractAudio',
-#             'preferredcodec': 'mp3',
-#             'preferredquality': '192',
-#         }],
-        'logger': MyLogger(),
-        'progress_hooks': [finished_hook],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_id])
-        
-    print('Now loading and predicting ...')
-    return(filename, status)
+    if (Path(folderpath)/(video_id+'.webm')).is_file():
+        print('Audio '+video_id+' already exists')
+        return folderpath+'/'+(video_id+'.webm'), 'exists'
+    else:
+        filename = 'nullname'
+        status = 'not_started'
+        def finished_hook(d):
+            nonlocal filename
+            nonlocal status
+            if d['status'] == 'finished':
+    #             print(filename)
+                filename = d['filename']
+                status = d['status']
+                print(f'Done downloading {filename}.')
+            
+        ydl_opts = {
+            'outtmpl': folderpath+'%(id)s.%(ext)s',
+            'format': 'worstaudio/worst',
+    #         'postprocessors': [{
+    #             'key': 'FFmpegExtractAudio',
+    #             'preferredcodec': 'mp3',
+    #             'preferredquality': '192',
+    #         }],
+            'logger': MyLogger(),
+            'progress_hooks': [finished_hook],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_id])
+            
+        print('Now loading and predicting ...')
+        return(filename, status)
 
      
